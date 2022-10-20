e41d6068f88a654b5b8b36573bdf152e375158a4
Isha Hitang <88145617+iamisha@users.noreply.github.com>
2022-10-20 23:31:51 +0545

    add files via upload

---
+++ day-3/JavaScript-Varibles/letandconst.js
@@ -0,0 +1,11 @@
+// Here are the using way of variables let and const.
+/*
+let fname = "Sahishna";
+let fname = "Isha";
+document.write(fname);/* it will throw an error 
+"Uncaught SyntaxError: Identifier 'fname' has already been declared" */
+
+const lname = "Budhathoki";
+lname = "Hitang";
+document.write(lname);/* this will throw an TypeError as 
+" Uncaught TypeError: Assignment to constant variable."*/
---
+++ day-3/JavaScript-Varibles/var.js
@@ -0,0 +1,22 @@
+var z = "JavaScript is client side scripting language<br>";
+
+z = "affable<br>";// it overrides the old value of the z and print the new value of z i.e the output will be no-way
+//no need to write var again as we have already declared above using var
+z = "hehe<br>"; // it will override the value affable and give the output hehe. if you don't believe please run it at once on your code editor
+document.write(z);
+
+/* Here z is the name of the variable and "JavaScript is client side scripting language"  is the value*/
+
+// can also have the numeric value but shouldn't use " " or ' '
+var x = 201710;
+document.write(x);
+
+/*var first_name = "Isha"
+var 99last_name = "Hitang"
+document.write(first_name);
+document.write(99last_name);*/
+//remove the codes that are giving error to get the expected output :)
+
+var first, second;
+first = 50;
+document.write(first);
