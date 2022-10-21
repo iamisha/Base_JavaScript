ca0dab451394fbd2e1ada94a9e2feb6305ce0b8f
Isha Hitang <88145617+iamisha@users.noreply.github.com>
2022-10-21 22:30:56 +0545

    datatype file added

---
+++ day-4/Js-datatypes/jsdatatype.js
@@ -0,0 +1,22 @@
+var x = 'JavaScript';
+x = 201710;
+x = 20.44;
+x = -20.44;
+x = false;
+x = 'false';
+x = undefined;
+x = ["tea", "cofee", "juice"];
+x = {
+    name: "Asha",
+    age: 20, 
+    address: "Kathmandu"
+};
+x = "25.5";
+x = 33n;
+x = () => 2 + 2;
+var y;
+document.write(x);
+document.write('<br>the type of data x is: ');
+document.write(typeof x);
+document.write('<br>the type of  data y is: ');
+document.write(typeof y);
